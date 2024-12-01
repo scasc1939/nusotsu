ef95003117ce4705aae07ca3df3dbf5eb0e7b6a6
scasc1939 <98072525+scasc1939@users.noreply.github.com>
2024-12-01 21:30:00 +0900

    fixed compile.bat / CR to CRLF

--- compile.bat
+++ compile.bat
@@ -3,3 +3,10 @@ latexmk -pdflua -r .latexmkrc
 pause
 :: 一時ファイルを消去
 latexmk -c
+
+:: github からダウンロードしたときに、
+:: 改行コードの関係で Windows Terminal がエラーを吐くようなので、
+:: git の設定を直しました。
+:: https://docs.github.com/ja/get-started/getting-started-with-git/configuring-git-to-handle-line-endings
+:: うまくできているかな？
+::（2024/12/01）
