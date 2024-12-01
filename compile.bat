3bc2c2f520d35a4c4194a8a36903a9f9c2802020
scasc1939 <98072525+scasc1939@users.noreply.github.com>
2024-12-01 21:23:03 +0900

    fixed compile.bat / CR to CRLF

--- compile.bat
+++ compile.bat
@@ -1,5 +1,13 @@
 :: LuaTeX でコンパイル
 latexmk -pdflua -r .latexmkrc
+:: いったん待つ（エラー発生時にログを読むため）
 pause
 :: 一時ファイルを消去
 latexmk -c
+
+:: github からダウンロードしたときに、
+:: 改行コードの関係で Windows Terminal がエラーを吐くようなので、
+:: git の設定を直しました。
+:: https://docs.github.com/ja/get-started/getting-started-with-git/configuring-git-to-handle-line-endings
+:: うまくできているかな？
+::（2024/12/01）
